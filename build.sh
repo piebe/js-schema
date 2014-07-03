2cfc4e537495d91e11c1c45216be67b78fec4776
piebev <piebe.de.vries@geodan.nl>
2014-07-03 22:44:34 +0200

    - fixed some  validation - fixed build script - added debug build

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 DEBUG="js-schema.debug.js"
 MIN="js-schema.min.js"
 
-browserify index.js | sed 's|require("/index.js")|window.schema = require("/index.js")|g' >$DEBUG
+browserify index.js -o $DEBUG -s schema
 
 uglifyjs $DEBUG >$MIN
